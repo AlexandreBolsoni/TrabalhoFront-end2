aeb587187df21138321d21f18218d3176210f28d
Alexandre Hackbardt Bolsoni <104595251+AlexandreBolsoni@users.noreply.github.com>
2023-06-26 17:42:37 -0300

    mudanças totas na estilização

--- src/paginas/SobreMim/index.jsx
+++ src/paginas/SobreMim/index.jsx
@@ -1,9 +1,19 @@
 
+import React from 'react';
+import styles from './SobreMim.module.css';
+
+
+
 function SobreMim() {
-    return (
-      <h1>Sobre Mim</h1>    
-    )
-  }
-  
-  export default SobreMim;
-  
+  return (
+    <div className={styles['sobre-mim']}>
+      <h1>Sobre Mim</h1>
+      <p>Olá, eu sou [Alexandre Hackbardt].</p>
+      <p>Sou apaixonado por desenvolvimento web e adoro criar coisas incríveis com tecnologia.</p>
+      <p>Tenho experiência em HTML, CSS e JavaScript, além de frameworks como React e Angular.</p>
+      <p>Estou sempre em busca de novos desafios e aprendendo coisas novas para aprimorar minhas habilidades.</p>
+    </div>
+  );
+}
+
+export default SobreMim;
